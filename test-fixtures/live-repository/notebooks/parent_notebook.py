16f5418407db474013b70c3aca60ea1c6d03b5fc
Ferro <ferronhooi@gmail.com>
2026-08-07 09:07:38 +0200

    add

--- test-fixtures/live-repository/notebooks/parent_notebook.py
+++ test-fixtures/live-repository/notebooks/parent_notebook.py
@@ -2,3 +2,7 @@
 
 CHILD_RUN = 'notebookutils.notebook.run("child_notebook", 60)'
 
+
+def get_child_run() -> str:
+    """Return the configured child notebook execution command."""
+    return CHILD_RUN
